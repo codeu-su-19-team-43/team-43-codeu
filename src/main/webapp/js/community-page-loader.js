9fddbb280c09880433ebe8a6cf8de2df1ae201b7
Anqi Tu <anqitu@outlook.com>
2019-05-25 21:02:42 +0800

    Split html and javascript into their own files

---
+++ src/main/webapp/js/community-page-loader.js
@@ -0,0 +1,30 @@
+// Fetched users and adds them to the page
+function fetchUserList() {
+  const url = "/user-list";
+  fetch(url).then((response) => {
+    return response.json();
+  }).then((users) => {
+    const list = document.getElementById('list');
+    list.innerHTML = '';
+
+    users.forEach((user) => {
+      const userListItem = buildUserListItem(user);
+      list.appendChild(userListItem);
+    });
+  });
+}
+
+// Builds a list element that contains a link to a user page
+function buildUserListItem(user) {
+  const userLink = document.createElement('a');
+  userLink.setAttribute('href', '/user-page.html?user=' + user);
+  userLink.appendChild(document.createTextNode(user));
+  const userListItem = document.createElement('li');
+  userListItem.appendChild(userLink);
+  return userListItem;
+}
+
+// Fetches data and populates the UI of the page.
+function buildUI(){
+  fetchUserList();
+}
