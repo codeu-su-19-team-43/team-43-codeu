9916e39032b937bee133cd13562640d10bbeb249
Aadyaa Maddi <e0072565@u.nus.edu>
2019-06-05 12:31:27 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-bash ./appcfg.sh --service_account_json_key_file=client-secret.json --version=$VERSION update target/codeu-starter-project-0.0.1-SNAPSHOT
-
-bash ./appcfg.sh --service_account_json_key_file=client-secret.json --version=$VERSION set_default_version target/codeu-starter-project-0.0.1-SNAPSHOT
+appcfg.sh --service_account_json_key_file=client-secret.json --version=$VERSION update target/codeu-starter-project-0.0.1-SNAPSHOT &&
+appcfg.sh --service_account_json_key_file=client-secret.json --version=$VERSION set_default_version target/codeu-starter-project-0.0.1-SNAPSHOT
