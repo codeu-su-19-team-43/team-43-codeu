4c4398c002bcc72e7cb12da459529469ae0b5425
amad-person <e0072565@u.nus.edu>
2019-06-09 16:37:16 +0800

    Fix indentation in PhotoCategoriesChartServlet.java

--- src/main/java/com/google/codeu/servlets/PhotoCategoriesChartServlet.java
+++ src/main/java/com/google/codeu/servlets/PhotoCategoriesChartServlet.java
@@ -1,58 +1,56 @@
 package com.google.codeu.servlets;
 
-
-import java.io.IOException;
-import java.util.Scanner;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Handles fetching resources for displaying the photo categories chart.
  */
 @WebServlet("/photo-categories-chart")
 public class PhotoCategoriesChartServlet extends HttpServlet {
-    private JsonArray photoCategoriesArray;
+  private JsonArray photoCategoriesArray;
 
-    @Override
-    public void init() {
-        photoCategoriesArray = new JsonArray();
-        Gson gson = new Gson();
-        Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/photo-categories.csv"));
-        scanner.nextLine(); // skips column names
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] cells = line.split(",");
+  @Override
+  public void init() {
+    photoCategoriesArray = new JsonArray();
+    Gson gson = new Gson();
+    Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/photo-categories.csv"));
+    scanner.nextLine(); // skips column names
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      String[] cells = line.split(",");
 
-            String currCategoryName = cells[1];
-            int currPhotoCount = Integer.parseInt(cells[2]);
+      String currCategoryName = cells[1];
+      int currPhotoCount = Integer.parseInt(cells[2]);
 
-            photoCategoriesArray.add(gson.toJsonTree(new photoCategory(currCategoryName, currPhotoCount)));
-        }
-        scanner.close();
+      photoCategoriesArray.add(gson.toJsonTree(new photoCategory(currCategoryName, currPhotoCount)));
     }
+    scanner.close();
+  }
 
-    /**
-     * Responds with data for the photo categories chart in JSON.
-     */
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json");
-        response.getOutputStream().println(photoCategoriesArray.toString());
+  /**
+   * Responds with data for the photo categories chart in JSON.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+    response.getOutputStream().println(photoCategoriesArray.toString());
+  }
+
+  private static class photoCategory {
+    String categoryName;
+    int photoCount;
+
+    private photoCategory(String categoryName, int photoCount) {
+      this.categoryName = categoryName;
+      this.photoCount = photoCount;
     }
-
-    private static class photoCategory {
-        String categoryName;
-        int photoCount;
-
-        private photoCategory(String categoryName, int photoCount) {
-            this.categoryName = categoryName;
-            this.photoCount = photoCount;
-        }
-    }
+  }
 }
