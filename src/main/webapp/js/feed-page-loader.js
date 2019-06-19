1a331ce054e7efb61d72aaa7f692931f94e4d039
Anqi Tu <anqitu@outlook.com>
2019-06-19 22:14:42 +0800

    Display selected image label

--- src/main/webapp/js/feed-page-loader.js
+++ src/main/webapp/js/feed-page-loader.js
@@ -1,9 +1,21 @@
 const urlParams = new URLSearchParams(window.location.search);
 const parameterImageLabel = urlParams.get('imageLabel');
 
+function fetchImageLabels() {
+  if (parameterImageLabel != null && parameterImageLabel !== '') {
+    const imageLabelContainer = document.getElementById('image-label-container');
+    const imageLabelButton = document.createElement('div');
+    imageLabelButton.innerHTML = `<button type="button" class="btn btn-info btn-sm">
+                                  <span>${parameterImageLabel} &times;</span>
+                                </button>`;
+    imageLabelContainer.appendChild(imageLabelButton);
+  }
+}
+
 // Fetch data and populate the UI of the page.
 // eslint-disable-next-line no-unused-vars
 function buildUI() {
+  fetchImageLabels();
   $.getScript('/js/message-loader.js', () => {
     if (parameterImageLabel != null) {
       // eslint-disable-next-line no-undef
