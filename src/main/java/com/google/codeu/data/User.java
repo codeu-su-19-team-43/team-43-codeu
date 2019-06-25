ad603269b7ecaa3b501666953108b18219f76e47
amad-person <e0072565@u.nus.edu>
2019-06-25 10:18:52 +0800

    Add initial version of favourite message backend

--- src/main/java/com/google/codeu/data/User.java
+++ src/main/java/com/google/codeu/data/User.java
@@ -1,5 +1,9 @@
 package com.google.codeu.data;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +16,6 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 public class User {
-  
   private String email;
   private String username;
   private String location;
@@ -20,4 +23,43 @@ public class User {
   private String website;
   private String aboutMe;
   private String profileImageUrl;
+  private List<UUID> favouriteMessages;
+
+  public User(String email, String username, String location, String organization,
+              String website, String aboutMe, String profileImageUrl) {
+    this(email, username, location, organization, website, aboutMe, profileImageUrl,
+            new ArrayList<>());
+  }
+
+  /**
+   * Converts list of favourite IDs as strings to list of favourite IDs as UUIDs.
+   */
+  public List<UUID> convertStringsToFavouriteIds(List<String> favouriteIdsAsStrings) {
+    if (favouriteIdsAsStrings == null) {
+      return new ArrayList<>();
+    }
+
+    List<UUID> favouriteIds = new ArrayList<>();
+    for (String favouriteIdAsString: favouriteIdsAsStrings) {
+      favouriteIds.add(UUID.fromString(favouriteIdAsString));
+    }
+
+    return favouriteIds;
+  }
+
+  /**
+   * Converts list of favourite IDs as UUIDs to list of favourite IDs as strings.
+   */
+  public List<String> convertfavouriteIdsToStrings(List<UUID> favouriteIds) {
+    if (favouriteIds == null) {
+      return new ArrayList<>();
+    }
+
+    List<String> favouriteIdsAsStrings = new ArrayList<>();
+    for (UUID favouriteId: favouriteIds) {
+      favouriteIdsAsStrings.add(favouriteId.toString());
+    }
+
+    return favouriteIdsAsStrings;
+  }
 }
