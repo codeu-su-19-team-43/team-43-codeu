b7ecb181fa4cd7a9c9c722c87c37320b9dfcdde5
Anqi Tu <anqitu@outlook.com>
2019-07-23 18:23:49 +0800

    Fix bugs when searching with special chars (#118)

--- src/main/webapp/js/feed-page-loader.js
+++ src/main/webapp/js/feed-page-loader.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const parameterSearchLabels = urlParams.getAll('searchLabel');
 
-function fetchSearchLabels() {
+function buildSearchLabels() {
   if (parameterSearchLabels != null) {
     parameterSearchLabels.forEach((parameterSearchLabel) => {
       const searchTagContainer = document.getElementById('search-tag-container');
@@ -18,10 +18,47 @@ function fetchSearchLabels() {
   }
 }
 
+function cleanWindowLocation(windowLocation) {
+  // eslint-disable-next-line no-useless-escape
+  return windowLocation.replace('\?&', '?').replace(/\?$/, '');
+}
+
+function checkUniqueSearchLabel(label) {
+  if (parameterSearchLabels.indexOf(label) > -1) {
+    return false;
+  }
+  return true;
+}
+
+// eslint-disable-next-line no-unused-vars
+function onClickSearchButton() {
+  const searchLabel = document.getElementById('search-input').value.toLowerCase();
+  if (checkUniqueSearchLabel(searchLabel)) {
+    let redirectLocation = window.location.href;
+    redirectLocation += redirectLocation.includes('?') ? '&' : '?';
+    redirectLocation += `searchLabel=${searchLabel}`;
+    window.location.replace(cleanWindowLocation(redirectLocation));
+  } else {
+    document.getElementById('search-input').value = '';
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+function onClickSearchLabelCancelButton(searchLabel) {
+  const newParameterSearchLabels = parameterSearchLabels
+    .filter(label => encodeURIComponent(label) !== searchLabel);
+  let redirectLocation = window.location.pathname;
+  newParameterSearchLabels.forEach((label, count) => {
+    redirectLocation += count === 0 ? '?' : '&';
+    redirectLocation += `searchLabel=${label}`;
+  });
+  window.location.replace(redirectLocation);
+}
+
 // Fetch data and populate the UI of the page.
 // eslint-disable-next-line no-unused-vars
 function buildUI() {
-  fetchSearchLabels();
+  buildSearchLabels();
   $.getScript('/js/message-loader.js', () => {
     if (parameterSearchLabels != null && parameterSearchLabels.length > 0) {
       // eslint-disable-next-line no-undef
